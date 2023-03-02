1002984faa83d394aaef90094cf83b90a75787d8
ban-works <alban.fresil@gmail.com>
2023-03-02 15:22:09 +0100

    ADD pages

---
+++ pages/bootleggers.tsx
@@ -0,0 +1,24 @@
+import styles from '../styles/Home.module.css'
+import Releases from "../components/Releases"
+import localFont from 'next/font/local'
+const myFont = localFont({ src: '../fonts/moonraze.otf' })
+import Image from "next/image"
+import {useEffect} from 'react'
+
+export default function BootleggersPage(props:any) {
+  useEffect(() => {
+    props.handleActivePageChange('BootleggersPage')
+
+  },[])
+  return (
+    <div className={styles.container} >
+      <main className={styles.main}>
+
+ <Releases/>
+
+      </main>
+
+
+    </div>
+  )
+}
