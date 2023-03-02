070866761621e6f4a1df482879beac83e03a2c9e
ban-works <alban.fresil@gmail.com>
2023-03-02 15:20:05 +0100

    ADD activePage icon color based on Layout.js prop

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,47 +1,110 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRecordVinyl, faHatWizard, faH } from '@fortawesome/free-solid-svg-icons'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-export default function Footer() {
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRecordVinyl, faHatWizard, faShirt } from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+type Props ={
+  activePage?:string
+}
+
+export default function Footer({activePage}:Props) {
+
+
+let firstInactiveClassName = "text-gator-green mr-3"
+let firstActiveClassName = "text-gator-pink mr-3"
+let inactiveClassName = "text-gator-green mr-3 ml-3"
+let activeClassName = "text-gator-pink mr-3 ml-3"
+let lastInactiveClassName = "text-gator-green mr-3 ml-3"
+let lastActiveClassName = "text-gator-pink mr-3 ml-3"
+
+let homeIconStyle = firstInactiveClassName
+if (activePage === "HomePage"){
+  homeIconStyle = firstActiveClassName
+}
+
+let releasesIconStyle = inactiveClassName
+if (activePage === "ReleasesPage"){
+  releasesIconStyle = activeClassName
+}
+
+let bootleggersIconStyle = inactiveClassName
+if (activePage === "BootleggersPage"){
+  bootleggersIconStyle = activeClassName
+}
+
+let merchIconStyle = lastInactiveClassName
+if (activePage === "MerchPage"){
+  merchIconStyle = lastActiveClassName
+}
+
+
   return (
-
-      <footer style={{height:'15vh', position: 'fixed', bottom: 0,width: '100%',backgroundColor:'#170C5A', zIndex:100}} className={styles.footer} >
-       <div className=" w-[50%] h-[10vh] flex justify-around ">
-       <a
-          href="/releases"
-
-          rel="noopener noreferrer"
-          style={{display:'flex', flexDirection:'column'}} className="text-gator-pink">
-            <FontAwesomeIcon icon={faRecordVinyl} size="lg"/>
-            Releases
-
-            </a>
-            <a
-          href="/"
-
-          rel="noopener noreferrer"
-          style={{display:'flex', flexDirection:'column'}} className="text-gator-pink">
-            <FontAwesomeIcon icon={faHatWizard} size="lg"/>
-
-Home
-            </a>
-       </div>
+    <footer
+      style={{
+        // height: "12vh",
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "#170C5A",
+        zIndex: 100,
+      }}
+      className={styles.footer}
+    >
+      <div className=" flex justify-around ">
         <a
-          href="https://ban-works.github.io"
-          target="_blank"
+          href="/"
           rel="noopener noreferrer"
-          // style={{display:'flex', flexDirection:'column'}}
-          className="text-gator-pink">
-          Made with <Image
-        src="/image50.png"
-        width={88}
-        height={50}
-        alt="Gator gizz logo : a green aligator"
-      /> by Ban
-
+          style={{ display: "flex", flexDirection: "column" }}
+          className={homeIconStyle}
+        >
+          <FontAwesomeIcon icon={faHatWizard} size="lg" />
+          Home
         </a>
-      </footer>
-
-  )
+        <a
+          href="/releases"
+          rel="noopener noreferrer"
+          style={{ display: "flex", flexDirection: "column" }}
+          className={releasesIconStyle}
+        >
+          <FontAwesomeIcon icon={faRecordVinyl} size="lg" />
+          Releases
+        </a>
+        <a
+          href="/bootleggers"
+          rel="noopener noreferrer"
+          style={{ display: "flex", flexDirection: "column" }}
+          className={bootleggersIconStyle}
+        >
+          <FontAwesomeIcon icon={faRecordVinyl} size="lg" />
+          Bootleggers
+        </a>{" "}
+        <a
+          href="/merch"
+          rel="noopener noreferrer"
+          style={{ display: "flex", flexDirection: "column" }}
+          className={merchIconStyle}
+        >
+          <FontAwesomeIcon icon={faRecordVinyl} size="lg" />
+          Merch
+        </a>
+      </div>
+      <a
+        href="https://ban-works.github.io"
+        target="_blank"
+        rel="noopener noreferrer"
+        // style={{display:'flex', flexDirection:'column'}}
+        className="text-gator-pink text-xs"
+      >
+        Made with
+        <Image
+          src="/image50.png"
+          width={44}
+          height={25}
+          alt="Gator gizz logo : a green aligator"
+        />{" "}
+        by Ban
+      </a>
+    </footer>
+  );
 }
