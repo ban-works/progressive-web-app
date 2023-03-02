494de7420f443455e8ee962ece7262f8db23f540
ban-works <alban.fresil@gmail.com>
2023-03-02 15:19:30 +0100

    ADD activePage prop from _app.js

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,17 +6,22 @@ import styles from "../styles/Home.module.css";
 import Header from "./Header";
 import Footer from "./Footer";
 
+
 type Props = {
   children?: ReactNode;
   title?: string;
+  activePage?:string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const Layout = ({ children, title = "This is the default title", activePage }: Props)=>  {
+console.log(activePage)
+    return  (
+        <div>
+          <Header />
+          {children}
+          <Footer activePage={activePage} />
+        </div>
+      );
+}
 
 export default Layout;
