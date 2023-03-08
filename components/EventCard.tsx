1efd1d4f900cbbf859d7b7129c755319d16cd7d4
ban-works <alban.fresil@gmail.com>
2023-03-08 12:59:02 +0100

    EDIT cards style w/o images

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -3,13 +3,13 @@ import { Event } from "./Home";
 import Image from "next/image";
 
 export default function EventCard(props: Event) {
-
+console.log(props.name);
     let time = 0;
   return (
-    <a href={props.bookingURL} target="_blank">
-    <div className="flex-initial  text-gator-neongreen hover:text-gator-pink justify-center items-center min-h-[320px] min-w-[300px] p-2 bg-gator-pink border-4 border-gator-neongreen  shadow hover:bg-gator-lightgreen dark:bg-gator-pink dark:order-gator-neongreen dark:hover:bg-gator-lightgreen mb-4 mr-4 ">
+    <a href={props.bookingURL} target="_blank" className="flex flex-row w-[100%]">
+    <div className="flex flex-col w-[100%]  text-gator-neongreen hover:text-gator-pink  p-2 bg-gator-pink border-4 border-gator-neongreen  shadow hover:bg-gator-lightgreen dark:bg-gator-pink dark:order-gator-neongreen dark:hover:bg-gator-lightgreen mb-4 mr-4 ">
 
-      <div style={{width: '100%', height: '200px', position: 'relative'}}>
+      {/* <div style={{width: '100%', height: '200px', position: 'relative'}}>
 
       <Image
         //    className="w-80"
@@ -22,10 +22,17 @@ export default function EventCard(props: Event) {
         fill
 
       />
-      </div>
-      <h3 className="font-black ">{props.name}</h3>
+      </div> */}
+      <h3 className="font-black grow ">{props.name}</h3>
+{/* {     props.name.length<50 && <br/>
+} */}
+<p className=" text-gator-blue font-black ">
+     {props.venue}
+      {/* ({props.countryCode}) */}
+      </p>
       <p className=" text-gator-blue font-black ">
-     {props.city} ({props.countryCode})
+     {props.city}
+      {/* ({props.countryCode}) */}
       </p>
       <p className="text-gator-blue">
         {props.localDate} {props.localTime && "//"} {props.localTime?.slice(0,5)}
