47e81f8983697d1c103326c423321bd65233631f
ban-works <alban.fresil@gmail.com>
2023-03-09 13:16:15 +0100

    EDIT GTM integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+    const GTM_ID = 'GTM-PFCM7MZ'
+
   return (
     <Html>
       <Head />
@@ -9,7 +11,7 @@ export default function Document() {
         <NextScript />
         <noscript
     dangerouslySetInnerHTML={{
-      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX" height="0" width="0" style="display: none; visibility: hidden;" />`,
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
     }}
   />
       </body>
