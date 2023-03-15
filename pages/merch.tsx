3515a0f985d2af4d2a403d3eaa6a6c7cdadc9a2c
ban-works <alban.fresil@gmail.com>
2023-03-15 20:05:19 +0100

    TEST upload camera

--- pages/merch.tsx
+++ pages/merch.tsx
@@ -3,23 +3,47 @@ import Releases from "../components/Releases"
 import localFont from 'next/font/local'
 const myFont = localFont({ src: '../fonts/moonraze.otf' })
 import Image from "next/image"
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 export default function MerchPage(props:any) {
+  const [file, setFile] = useState('');
   useEffect(() => {
     props.handleActivePageChange('MerchPage')
 
   },[])
+
+  const handleUpload = (e:any) => {
+    if (e.target.files){
+      const file = e.target.files[0]
+      console.log(file);
+      const newUrl = URL.createObjectURL(file);
+      console.log(newUrl);
+      setFile(newUrl)
+    }
+    console.log(e.target.files)
+  }
+
+  // const [source, setSource] = useState("");
+  // const handleCapture = (target) => {
+  //   if (target.files) {
+  //     if (target.files.length !== 0) {
+  //       const file = target.files[0];
+  //       const newUrl = URL.createObjectURL(file);
+  //       setSource(newUrl);
+  //     }
+  //   }
+  // };
   return (
     <div className={styles.container} >
       <main className={styles.main}>
 <h1 className="text-3xl font-extrabold m-5 ">This is a test for camera access</h1>
-      <input style={{display:"none"}}accept="image/*" id="icon-button-file" type="file" capture="environment"/>
+      <input style={{display:"none"}}accept="image/*" id="icon-button-file" type="file" capture="environment" onChange={(e)=>handleUpload(e)}/>
       <label htmlFor="icon-button-file" className="bg-gator-blue text-gator-neongreen p-2 font-bold">
 Scanner mon document         </label>
+
+{ file && <img src={file} alt={"snap"}></img>}
+
       </main>
-
-
     </div>
   )
 }
