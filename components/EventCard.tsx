ca157034b882ce910252567a443015fe95cb630e
ban-works <alban.fresil@gmail.com>
2023-03-24 16:57:56 +0100

    ADD button

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Event } from "./Home";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHatWizard,
+} from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export default function EventCard(props: Event) {
   return (
     <a href={props.bookingURL} target="_blank" className="flex flex-row w-[100%]">
-    <div className="flex flex-col w-[100%]  text-gator-neongreen hover:text-gator-pink  p-2 bg-gator-pink border-4 border-gator-neongreen  shadow hover:bg-gator-lightgreen dark:bg-gator-pink dark:order-gator-neongreen dark:hover:bg-gator-lightgreen mb-4 mr-4 ">
+    <div className=" pl-6 pr-6 flex flex-col w-[100%]  text-gator-neongreen hover:text-gator-pink  p-2 bg-gator-pink border-4 border-gator-neongreen  shadow hover:bg-gator-lightgreen dark:bg-gator-pink dark:order-gator-neongreen dark:hover:bg-gator-lightgreen mb-4 mr-4 ">
 
       {/* <div style={{width: '100%', height: '200px', position: 'relative'}}>
 
@@ -20,7 +25,7 @@ export default function EventCard(props: Event) {
 
       />
       </div> */}
-      <h3 className="font-black grow ">{props.name}</h3>
+      <h3 className="font-black grow pt-4 ">{props.name}</h3>
 {/* {     props.name.length<50 && <br/>
 } */}
 <p className=" text-gator-blue font-black ">
@@ -34,6 +39,9 @@ export default function EventCard(props: Event) {
       <p className="text-gator-blue">
         {props.localDate} {props.localTime && "//"} {props.localTime?.slice(0,5)}
       </p>
+      <button className=" hover:text-gator-blue rounded-lg mt-2 text-l text-left hover:text-xl pb-5 hover:pb-4">
+          <FontAwesomeIcon icon={faHatWizard}/> BOOK TICKETS
+        </button>
 
     </div>
     </a>
